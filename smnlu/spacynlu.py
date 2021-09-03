506d7174767af727c3c6410a2ba20324cad9ac2f
Ivan Averin <i.averin@gmail.com>
2021-09-03 15:26:46 +0000

    get intent from utterance

--- smnlu/spacynlu.py
+++ smnlu/spacynlu.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from typing import Counter, TypedDict
+from typing import Counter, List, TypedDict, Union
 import spacy
 from spacy.matcher import Matcher
 
@@ -8,28 +8,61 @@ from spacy.matcher import Matcher
 #     intent_id: TypedDict("Intent", {"id":str, "confidence":float })
 
 
+class Intent():
+    PATTERN_STOP_SUFFIX = "_int_stop"
+
+    def __init__(self, id: str, patterns_match: List, patterns_stop: List):
+        self.id = id
+        self.patterns_match = patterns_match
+        self.patterns_stop = patterns_stop
+
+    def match_pattern_id(self):
+        return self.id
+
+    def stop_patten_id(self):
+        return self.id+self.PATTERN_STOP_SUFFIX
+    
+    def fits_patterns(self, matched_patterns: List[str]) -> bool:
+        if self.match_pattern_id() in matched_patterns and \
+            self.stop_patten_id() not in matched_patterns:
+            return True
+        
+        return False
+
+
+
 class SpacyNlu:
-    def __init__(self, model: str) -> None:
+
+    def __init__(self, model: str, intents: List[Intent]) -> None:
         self.nlp = spacy.load(model)
         self.matcher = Matcher(self.nlp.vocab)
+        self.intents: List[Intent] = self._init_intents(intents)
 
-    def set_patterns(self, intent: str, pattern_match: list, pattern_stop: list) -> dict:
-        self.matcher.add(intent, pattern_match)
-        self.matcher.add(intent+'_stop', pattern_stop)
+    def intent(self, utterance: str) -> Union[Intent, None]:
+        matched_patterns = self._get_matched_patterns(utterance)
+        for intent in self.intents:
+            if intent.fits_patterns(matched_patterns):
+                return intent
+        return None
 
-    def get_intents_weightned(self, utterance: str) -> dict:
+    # private supplementary methods 
+    def _init_intents(self, intents: List[Intent]) -> List[Intent]:
+        for intent in intents:
+            self.matcher.add(intent.match_pattern_id(), intent.patterns_match)
+            self.matcher.add(intent.stop_patten_id(), intent.patterns_stop)
+        return intents
+    
+    def _get_matched_patterns(self, utterance: str) -> list[str]:
         doc = self.nlp(utterance)
         matches = self.matcher(doc)
+        matched_patterns = list()
+        for match in matches:
+            pattern_id = self.nlp.vocab.strings[match[0]]
+            if pattern_id not in matched_patterns:
+                matched_patterns.append(pattern_id)
 
-        intents_counter = Counter()
-        intents_weight = dict()
+        return matched_patterns
 
-        for match_id, start, end in matches:
-            intent_id = self.nlp.vocab.strings[match_id]
-            intents_counter.update({intent_id})
-            intents_weight[intent_id] = intents_counter[intent_id] / sum(intents_counter.values())
-
-        return intents_weight
 
 
 # nlp = spacy.load("ru_core_news_sm")
--- tests/smnlu_spacy_nlu_test.py
+++ tests/smnlu_spacy_nlu_test.py
@@ -1,20 +1,41 @@
-from smnlu.spacynlu  import SpacyNlu
+from smnlu.spacynlu import Intent, SpacyNlu
 import unittest
 
 
 class SpacyNluTestSuite(unittest.TestCase):
     def setUp(self):
-        self.spacy_nlu = SpacyNlu("ru_core_news_sm")
 
-    def test_get_intent(self):
-        print("E")
-        self.spacy_nlu.set_patterns("test", [[{"lower":"test"}]], list())
-        self.spacy_nlu.set_patterns("me", [[{"lower":"me"}]], list())
+        intents = [
+            Intent(
+                id="test",
+                patterns_match=[[{"lower": "test"}]],
+                patterns_stop=[[{"lower": "error"}]]
+            ),
+            Intent(
+                id="lorem_ipsum",
+                patterns_match=[[{"lower": "lorem"}]],
+                patterns_stop=[]
+            )
 
-        intents = self.spacy_nlu.get_intents_weightned("Test this utterance for me. Test that.")
+        ]
 
-        print(intents)
+        self.spacy_nlu = SpacyNlu("ru_core_news_sm", intents=intents)
 
-      
-    
+    def test_intent(self):
 
+        intent = self.spacy_nlu.intent(
+            "Test this utterance for me. Test that.")
+        self.assertEqual("test", intent.id)
+
+        intent = self.spacy_nlu.intent(
+            "Test this utterance for me. Test that. Error.")
+        self.assertIsNone(intent)
+
+        intent = self.spacy_nlu.intent(
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, \
+             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
+
+        self.assertEqual("lorem_ipsum", intent.id)
+
+        intent = self.spacy_nlu.intent("Blah blah blah")
+        self.assertIsNone(intent)
